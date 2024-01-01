16aeece920f102e830ab7837126e69aa67e932d1
tim <timdesmet00@gmail.com>
2024-01-01 18:15:09 +0100

    ajout de lien

--- resources/views/component/footer.blade.php
+++ resources/views/component/footer.blade.php
@@ -1,5 +1,5 @@
 <footer>
-    <a href="#">github</a>
+    <a href="https://github.com/TimDesmet00">github</a>
     <span> Créer par Tim Desmet en 2023</span>
-    <a href="#">linkedin</a>
+    <a href="https://www.linkedin.com/in/tim-desmet-dev/">linkedin</a>
 </footer>
